4baa58d2191a7b8d38d9042e84f377c886a8b3e7
sai kamireddi <111673552+saidurga4521@users.noreply.github.com>
2025-03-31 13:48:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function basic(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!")
+		},1000)
+	})
+}
+const outputE1=document.getElementById('output');
+basic().then((res)=>{
+	outputE1.textContent=res;
+})
